2d9986fa9cbff8dc879ffb74bbd630860096cc2f
Tom Hoglund <infrapale@gmail.com>
2024-10-09 18:32:05 +0300

    adding aio

---
+++ T2410_Ruuvi_AIO_Gateway2/main.h
@@ -0,0 +1,15 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#define NBR_SENSORS               4       ///< Number of sensor values
+#define BLE_VERBOSE               false
+
+typedef struct 
+{
+    uint16_t  state;
+    uint32_t  next_ble_scan_ms;
+    uint32_t  next_print_ms;
+    uint8_t   sensor_indx;
+} main_ctrl_st;
+
+#endif
